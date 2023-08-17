9c8b633fe7f242abd34eac696155268a1081942a
Michael-Vol <mivolakis@gmail.com>
2023-08-17 20:25:10 +0300

    feat(security): implement jwt auth filter

--- src/main/java/com/michaelvol/ecommerceapi/security/JWTService.java
+++ src/main/java/com/michaelvol/ecommerceapi/security/JWTService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
 
 import java.security.Key;
 import java.util.Date;
@@ -16,6 +17,7 @@ import java.util.function.Function;
 
 import static com.michaelvol.ecommerceapi.utils.AppConstants.JWT_TOKEN_VALIDITY;
 
+@Service
 public class JWTService {
     @Value("${ecommerce.api.jwtSecret}")
     private String secretKey;
--- src/main/java/com/michaelvol/ecommerceapi/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/michaelvol/ecommerceapi/security/filter/JwtAuthenticationFilter.java
@@ -1,9 +1,16 @@
 package com.michaelvol.ecommerceapi.security.filter;
 
+import com.michaelvol.ecommerceapi.security.JWTService;
+import com.michaelvol.ecommerceapi.user.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,20 +18,47 @@ import java.io.IOException;
 
 
 @Component
+@AllArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JWTService jwtService;
+    private final UserService userService;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
         final String jwtToken;
         final String email;
+        final boolean isTokenValid;
 
         final String authenticationHeader = request.getHeader("Authorization");
 
-        if (authenticationHeader == null || !authenticationHeader.startsWith("Bearer ")) {
+        // Token Existence Check
+        if (authenticationHeader == null || !authenticationHeader.startsWith("Bearer "))
             filterChain.doFilter(request, response);
-        }
-        jwtToken = authenticationHeader.substring(7);
+        // Security Context Check
+        if (SecurityContextHolder.getContext().getAuthentication() != null)
+            filterChain.doFilter(request, response);
 
+
+        jwtToken = authenticationHeader.substring(7);
+        email = jwtService.getUsername(jwtToken);
+        // Email Existence Check
+        if (email == null)
+            filterChain.doFilter(request, response);
+
+        //Token Validity Check
+        isTokenValid = jwtService.isTokenValid(jwtToken, email);
+        if (!isTokenValid)
+            filterChain.doFilter(request, response);
+
+        // Set Authentication
+        UserDetails userDetails = userService.loadUserByUsername(email);
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(email, null, userDetails.getAuthorities());
+        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+        filterChain.doFilter(request, response);
 
     }
 }
