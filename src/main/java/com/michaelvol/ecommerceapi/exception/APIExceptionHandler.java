e240eadc2235abaf6bbf06b08ad41a459b1b730e
Michael-Vol <mivolakis@gmail.com>
2023-08-17 15:05:17 +0300

    feat(exceptions): added notfound exception

--- src/main/java/com/michaelvol/ecommerceapi/exception/APIExceptionHandler.java
+++ src/main/java/com/michaelvol/ecommerceapi/exception/APIExceptionHandler.java
@@ -2,6 +2,7 @@ package com.michaelvol.ecommerceapi.exception;
 
 import com.michaelvol.ecommerceapi.exception.exceptions.APIException;
 import com.michaelvol.ecommerceapi.exception.exceptions.BadRequestException;
+import com.michaelvol.ecommerceapi.exception.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,4 +23,17 @@ public class APIExceptionHandler {
                 .build();
         return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleAPIRequestException(NotFoundException exception) {
+        APIException apiException = APIException.builder()
+                .message(exception.getMessage())
+                .status(HttpStatus.NOT_FOUND)
+                .cause(exception)
+                .timestamp(ZonedDateTime.now())
+                .build();
+        return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+    }
+
+
 }
